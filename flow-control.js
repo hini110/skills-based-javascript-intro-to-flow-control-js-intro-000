943936642c89a34a29604e9fd109a5884cc43738
stella hong <hini110@hotmail.com>
2021-02-10 05:30:21 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,9 +1,13 @@
 function basicTeenager(age) {
-
+if ('age is between 13-19')
+return ('You are a teenager!')
 }
 
 function teenager(age) {
-
+if ('age is between 13-19')
+return('You are a teenager!')
+if('age is not between 13-19')
+return('You are not a teenager!')
 }
 
 function ageChecker(age) {
