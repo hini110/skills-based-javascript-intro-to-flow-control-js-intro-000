f4c025be232961737de195c9569b06161402ba97
stella hong <hini110@hotmail.com>
2021-02-10 05:32:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -4,11 +4,11 @@ return ('You are a teenager!')
 }
 
 function teenager(age) {
-if ('age is between 13-19')
-return('You are a teenager!')
-if('age is not between 13-19')
+if ('age is between 13-19'){
+return('You are a teenager!');}
+if('age is not between 13-19'){
 return('You are not a teenager!')
-}
+};
 
 function ageChecker(age) {
 
